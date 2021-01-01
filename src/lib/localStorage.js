c6717caf1571b359a88fe478a944d7c584404cdb
NakanoYuki <totoro.windows@outlook.com>
2021-01-01 21:59:45 +0900

    add LocalStorage Controller

---
+++ src/lib/localStorage.js
@@ -0,0 +1,37 @@
+class LocalStorage {
+  constructor() {
+    this.data = {}
+  }
+
+  init() {
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i)
+      this.data[key] = JSON.parse(localStorage.getItem(key)) || []
+    }
+  }
+
+  get(key) {
+    this.init()
+    return JSON.parse(this.data[key]) || []
+  }
+
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  remove(key) {
+    localStorage.removeItem(key)
+  }
+
+  clear() {
+    localStorage.clear()
+  }
+
+  all() {
+    this.init()
+    return this.data
+  }
+}
+
+const localstorage = new LocalStorage()
+export default localstorage
